407d08ddaf03311d9e1f43482c00b5da528a2739
kissdapp <kissdapp@gmail.com>
2019-05-26 21:35:23 +0900

    Updates

---
+++ precache-manifest.217f6495f61c83cf1f091ab60790eb48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a62285ec5987cbc125559a556b652ea",
+    "url": "/kiss/index.html"
+  },
+  {
+    "revision": "4ab6460de30d4530273b",
+    "url": "/kiss/static/css/main.ff0ef8f3.chunk.css"
+  },
+  {
+    "revision": "d8e0f837f71d86317781",
+    "url": "/kiss/static/js/2.1934d723.chunk.js"
+  },
+  {
+    "revision": "4ab6460de30d4530273b",
+    "url": "/kiss/static/js/main.f15ab2a0.chunk.js"
+  },
+  {
+    "revision": "c965231077e9eacc4239",
+    "url": "/kiss/static/js/runtime~main.f35041ba.js"
+  }
+]);
