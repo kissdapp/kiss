416a81b591aad7585088b8e19ddf4e7b3a2708c1
kissdapp <kissdapp@gmail.com>
2019-05-28 23:35:18 +0900

    commit

---
+++ build/precache-manifest.b03b5b4a81b797065ccd60a93b76a22c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0f6723ffc8fa4fcf6fc416398e8971",
+    "url": "/kiss/index.html"
+  },
+  {
+    "revision": "b6f05815ba3c05f9bc4e",
+    "url": "/kiss/static/css/main.ff0ef8f3.chunk.css"
+  },
+  {
+    "revision": "d8e0f837f71d86317781",
+    "url": "/kiss/static/js/2.1934d723.chunk.js"
+  },
+  {
+    "revision": "b6f05815ba3c05f9bc4e",
+    "url": "/kiss/static/js/main.8edc6ee3.chunk.js"
+  },
+  {
+    "revision": "c965231077e9eacc4239",
+    "url": "/kiss/static/js/runtime~main.f35041ba.js"
+  }
+]);
